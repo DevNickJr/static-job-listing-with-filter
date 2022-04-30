5e270f4737f09487f0549a8a57555683ac75f159
Nicholas Duadei <nicholasduadei14@gmail.com>
2022-05-01 00:01:35 +0100

    filter with js implemented

--- main.js
+++ main.js
@@ -1,12 +1,40 @@
 
-const filter = () => {
-    const filterList = document.getElementById('filter-items');
+const filter = async () => {
+    const filterDiv = document.getElementById('filter-list').children;
+    const filterList = [];
 
-}
+    if (!filterDiv.length==0) {
+        for (let container of filterDiv) {         
+            filterList.push(container.children[0].innerHTML);
+        }
+    }
 
-async function displayer() {
     let file = await fetch('./data.json');
     jobs = await file.json()
+    filteredJobs = []
+
+
+    for (let job of jobs) {
+        let state = true
+        const requirements = job.languages.concat(job.tools, job.role, job.level);
+        for (let filter of filterList) {
+            if (!requirements.includes(filter)) {
+                state = false
+            }
+        }
+        if (state) {
+            filteredJobs.push(job)
+        }
+    }
+    displayer(filteredJobs)
+    clicks()
+
+}
+filter()
+
+function displayer(jobs) {
+    let cardContainer = document.getElementById('card-container');
+    cardContainer.textContent = ''
 
     for (let job of jobs) {
         const card = document.createElement(`div`);
@@ -41,18 +69,53 @@ async function displayer() {
         cardExpertise.classList.add('card__expertise');
         cardExpertise.innerHTML = 
         `
-        <span class="card__role">${job.role}</span>
-        <span class="card__level">${job.level}</span>
+        <span class="card__role filter">${job.role}</span>
+        <span class="card__level filter">${job.level}</span>
         `
         const requirements = job.languages.concat(job.tools)
         for (let requirement of requirements) {
-            cardExpertise.innerHTML += `<span class="card__language">${requirement}</span>`
+            cardExpertise.innerHTML += `<span class="card__language filter">${requirement}</span>`
         }
         card.appendChild(cardExpertise)
 
-        document.getElementById('main').appendChild(card)
-    }
- 
+       
+        cardContainer.appendChild(card)
+    } 
 }
 
-displayer()
+function clicks() {
+    let options = document.querySelectorAll('.filter');
+    options.forEach(el => {
+        el.addEventListener('click', stope)
+    })
+}
+
+
+function stope() {
+    const filterList = document.getElementById('filter-list');
+    const nodes = filterList.children;
+    const filterItem = this.cloneNode(true);
+    // filterItem.classList.add('filt')
+    const remove = document.createElement('div')
+    remove.classList.add('remove')
+    const container = document.createElement('div')
+    container.classList.add('filter__div');
+    container.appendChild(filterItem);
+    container.appendChild(remove);
+    
+    if (!nodes.length) {
+        filterList.appendChild(container);
+    }
+    else {
+        let state = true
+        for (let node of nodes) {
+            if (node.children[0].innerHTML===this.innerHTML) {
+                state = false
+            }
+        }
+        if (state) {
+            filterList.appendChild(container);
+        }
+    }
+    filter()
+}
